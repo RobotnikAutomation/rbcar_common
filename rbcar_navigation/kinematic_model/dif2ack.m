4dfc02638702a32429ad0114dc4f42f567ba6fe2
juanma <jmnavarrete@robotnik.es>
2020-05-25 18:25:50 +0200

    new branch to melex

--- rbcar_navigation/kinematic_model/dif2ack.m
+++
@@ -1,4 +0,0 @@
-function [v_ack, a_ack] = dif2ack(v_twist, w_twist, d_ack)
-
-v_ack = sqrt(v_twist*v_twist + w_twist*w_twist*d_ack*d_ack);
-a_ack = atan(d_ack * w_twist / v_twist) ;
--- rbcar_navigation/kinematic_model/round2.m
+++
@@ -1,6 +0,0 @@
-function m = round2(n, dec)
-
-r = abs(rem(n, dec));
-suma = r > (dec/2);
-
-m = n - r + suma * dec;
